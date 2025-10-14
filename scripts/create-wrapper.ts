83e27c09b14fa75c72590ccbad8fc2bd31504adc
Craig Haseler <cahaseler@gmail.com>
2025-10-14 19:56:35 -0400

    fix: remove self-dependency and fix lint errors in wrapper script

--- scripts/create-wrapper.ts
+++ scripts/create-wrapper.ts
@@ -1,5 +1,5 @@
-import { writeFileSync } from 'fs';
-import { join } from 'path';
+import { writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 const wrapper = `#!/usr/bin/env node
 import { spawnSync } from 'child_process';
